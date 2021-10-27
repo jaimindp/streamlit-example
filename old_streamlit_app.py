c2a8df99cf23016cf61950e248b2897ac1e9ec25
jaimindp <jaiminpa@usc.edu>
2021-10-28 00:57:25 +0100

    uploaded ting and running

--- old_streamlit_app.py
+++ old_streamlit_app.py
@@ -1,38 +1,104 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import os
 
-"""
-# Welcome to Streamlit!
+def formatfunc(*args, **kwargs):
+    value = args[0]
+    if value >= 0:
+        return '${:,.2f}'.format(value)
+    else:
+        return '-${:,.2f}'.format(abs(value))
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+df = pd.read_csv('anon_trades_26_10_21.csv', error_bad_lines=False)
+my_df = df[df.trade_time.str.startswith('2')]
+closed = my_df.dropna(subset=['buy_timestamp'])
+closed.twitter_screen_name = '@'+closed.twitter_screen_name
+mean_group = closed.groupby('twitter_screen_name')['twitter_screen_name', 'percent_pnl'].agg('mean')
+sum_group  = closed.groupby('twitter_screen_name')['twitter_screen_name', 'dollar_pnl'].agg('sum')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+disp_df = mean_group.join(sum_group)
+disp_df.columns = ['Avg PnL', 'Total PnL']
+disp_df.index.name = 'Twitter Handle'
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.title("LazyTrade Leaderboard")
+st.subheader('Leaderboard')
 
+st.dataframe(disp_df.style.format(formatter={'Avg PnL': "{:.2f}%", 'Total PnL': formatfunc}), width=500, height=1000)
+# st.table(disp_df.style.format(formatter={'Avg PnL': "{:.2f}%", 'Total PnL': formatfunc}))
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.subheader('Historical Signals')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+users = st.multiselect('Choose users to track ', tuple(
+['@binance',
+ '@Bitstamp',
+ '@bloomberg_crypto',
+ '@coinbase',
+ '@CoinDesk',
+ '@coinmetrics',
+ '@crypto_rand',
+ '@cryptocom',
+ '@CryptoGodJohn',
+ '@CryptoKaleo',
+ '@CryptoMichNL',
+ '@cryptonews',
+ '@CryptoPanicCom',
+ '@cz_binance',
+ '@elonmusk',
+ '@KoroushAK',
+ '@kucoincom',
+ '@mcuban',
+ '@Poloniex',
+ '@Raticoin1',
+ '@RookieXBT',
+ '@TheCryptoDog',
+ '@thescalpingpro',
+ '@Tradermayne',
+ '@whale_alert',
+ '@Whale_Sniper',
+ '@WhaleTrades']
+))
 
-    points_per_turn = total_points / num_turns
+# if len(users) > 0:
+#     st.write('Selected')
+    
+images = os.listdir('images')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+import matplotlib.pyplot as plt
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+for user in users:
+    st.write(user)
+
+    coin_list = ['1INCH',
+                 'ADA',
+                 'ATOM',
+                 'AXS',
+                 'BNB',
+                 'BTC',
+                 'DOGE',
+                 'DOT',
+                 'ETH',
+                 'LINK',
+                 'LTC',
+                 'LUNA',
+                 'RUNE',
+                 'SOL',
+                 'SUSHI'
+                 'UNI',
+                 'XRP']
+    
+    all_coins = [i[len(user):].split('_')[0] for i in list(filter(lambda x : x.startswith(user[1:]), images))]
+    coin_subset = [i for i in coin_list if i in all_coins]
+        
+    coins = st.multiselect('Choose coins to plot', coin_subset)
+
+    for coin in coins:
+        st.write('User: %s\n\nCoin: %s' % (user, coin))
+#         st.image('images/%s_%s_BTC.png' % (user[1:], coin))
+        
+        HtmlFile = open('images/%s_%s_BTC.html' % (user[1:], coin), 'r', encoding='utf-8')
+        source_code_2 = HtmlFile.read()
+        components.html(source_code_2, height=700)
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ import streamlit.components.v1 as components
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import os
+
+st.set_page_config(layout='wide')
 
 def formatfunc(*args, **kwargs):
     value = args[0]
@@ -24,76 +27,29 @@ disp_df = mean_group.join(sum_group)
 disp_df.columns = ['Avg PnL', 'Total PnL']
 disp_df.index.name = 'Twitter Handle'
 
+images = os.listdir('images')
+
 st.title("LazyTrade Leaderboard")
 st.subheader('Leaderboard')
 
-st.dataframe(disp_df.style.format(formatter={'Avg PnL': "{:.2f}%", 'Total PnL': formatfunc}), width=500, height=1000)
+col1, col2, col3 = st.columns([1,1,1])
+
+col2.dataframe(disp_df.style.format(formatter={'Avg PnL': "{:.2f}%", 'Total PnL': formatfunc}), width=800, height=1000)
+
 # st.table(disp_df.style.format(formatter={'Avg PnL': "{:.2f}%", 'Total PnL': formatfunc}))
 
 st.subheader('Historical Signals')
 
-users = st.multiselect('Choose users to track ', tuple(
-['@binance',
- '@Bitstamp',
- '@bloomberg_crypto',
- '@coinbase',
- '@CoinDesk',
- '@coinmetrics',
- '@crypto_rand',
- '@cryptocom',
- '@CryptoGodJohn',
- '@CryptoKaleo',
- '@CryptoMichNL',
- '@cryptonews',
- '@CryptoPanicCom',
- '@cz_binance',
- '@elonmusk',
- '@KoroushAK',
- '@kucoincom',
- '@mcuban',
- '@Poloniex',
- '@Raticoin1',
- '@RookieXBT',
- '@TheCryptoDog',
- '@thescalpingpro',
- '@Tradermayne',
- '@whale_alert',
- '@Whale_Sniper',
- '@WhaleTrades']
-))
-
-# if len(users) > 0:
-#     st.write('Selected')
-    
-images = os.listdir('images')
-
-import matplotlib.pyplot as plt
+users = st.multiselect('Choose users to track ', tuple(['@binance', '@CoinDesk', '@CryptoKaleo', '@cz_binance', '@TheCryptoDog', 
+                                                        '@thescalpingpro', '@Whale_Sniper', '@WhaleTrades']), default=['@CoinDesk'])
 
 for user in users:
-    st.write(user)
-
-    coin_list = ['1INCH',
-                 'ADA',
-                 'ATOM',
-                 'AXS',
-                 'BNB',
-                 'BTC',
-                 'DOGE',
-                 'DOT',
-                 'ETH',
-                 'LINK',
-                 'LTC',
-                 'LUNA',
-                 'RUNE',
-                 'SOL',
-                 'SUSHI'
-                 'UNI',
-                 'XRP']
+    coin_list = ['1INCH','ADA','ATOM','AXS','BNB','BTC','DOGE','DOT','ETH','LINK','LTC','LUNA','RUNE','SOL','SUSHI''UNI','XRP']
     
     all_coins = [i[len(user):].split('_')[0] for i in list(filter(lambda x : x.startswith(user[1:]), images))]
     coin_subset = [i for i in coin_list if i in all_coins]
         
-    coins = st.multiselect('Choose coins to plot', coin_subset)
+    coins = st.multiselect('Choose coins to plot', coin_subset, default=['LTC'])
 
     for coin in coins:
         st.write('User: %s\n\nCoin: %s' % (user, coin))
